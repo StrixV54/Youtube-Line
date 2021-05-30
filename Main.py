457716cbb0c4e1e899900016de5e682ab6c2535a
StrixV54 <64365738+StrixV54@users.noreply.github.com>
2021-05-30 11:07:22 +0530

    update player

--- Main.py
+++ Main.py
@@ -5,10 +5,11 @@ import time
 import pafy
 from pytube import YouTube
 from tkinter import messagebox,filedialog
+from youtubesearchpython import VideosSearch
 
 root=Tk()
-root.geometry("500x110")
-root.resizable(0,0)
+root.geometry("550x110")
+#root.resizable(0,0)
 root.title("Youtube Video Downloader")
 root.config(background="#000000")
 video_Link=StringVar()
@@ -16,12 +17,24 @@ download_Path=StringVar()
 
 def Vplayer(url):
     url = "https://www.youtube.com/watch?v=nfs8NYg7yQM"
+    videosSearch = VideosSearch('Attention', limit = 5)
+    for i in range(5):
+        print(videosSearch.result()['result'][i]['link'])
     video = pafy.new(url)
     best = video.getbest()
     media = vlc.MediaPlayer(best.url)
     media.play()
-    print("Hello2")
-    time.sleep(10) 
+    time.sleep(10)
+
+def callSearch():
+    titleList=[];
+    linkList=[];
+    videosSearch = VideosSearch('Attention', limit = 6)
+    for i in range(6):
+        titleList.append(videosSearch.result()['result'][i]['title'])
+        linkList.append(videosSearch.result()['result'][i]['link'])
+    for i in range(5):
+        print(titleList[i])
 
 def Browse():
     download_Directory=filedialog.askdirectory(
@@ -49,14 +62,57 @@ def Widgets():
     destiText=Entry(root,width=40,textvariable=download_Path)
     destiText.grid(row=2,column=1,pady=5,padx=5) 
 
-    browse_B=Button(root,text="Browse",command=Browse,width=10,bg="#05E8E0")
-    browse_B.grid(row=2,column=2,pady=1,padx=1)
+    browse_B=Button(root,text="Browse",command=Browse,width=15,bg="#05E8E0")
+    browse_B.grid(row=2,column=2,pady=5,padx=5)
 
-    Search_B=Button(root,text="Search",width=10,bg="#05E8E0")
-    Search_B.grid(row=1,column=2,pady=1,padx=1)
+    Search_B=Button(root,text="Search",command=WidgetResult,width=15,bg="#05E8E0")
+    Search_B.grid(row=1,column=2,pady=5,padx=5)
 
-    Download_B=Button(root,text="Download",command=Download,width=20,bg="#05E8E0")
-    Download_B.grid(row=3,column=1,pady=3,padx=3)
+def WidgetResult():
+    root.geometry("550x300")
+    colrLab="#cccccc"
+    colrBut="#fd9c35"
+    
+    callSearch();
+
+    resultLabel_1=Label(root, text="1 Youtube Search",bg=colrLab,width=50)      #1
+    resultLabel_1.grid(row=3,columnspan=2,pady=5,padx=5)
+
+    Search_B=Button(root,text="Play",width=10,bg=colrBut)
+    Search_B.grid(row=3,column=2,pady=1,padx=1)
+
+    resultLabel_1=Label(root, text="Youtube Search",bg=colrLab,width=50)      #2
+    resultLabel_1.grid(row=4,columnspan=2,pady=5,padx=5)
+
+    Search_B=Button(root,text="Play",width=10,bg=colrBut)
+    Search_B.grid(row=4,column=2,pady=1,padx=1)
+
+    resultLabel_1=Label(root, text="Youtube Search",bg=colrLab,width=50)      #3
+    resultLabel_1.grid(row=5,columnspan=2,pady=5,padx=5)
+
+    Search_B=Button(root,text="Play",width=10,bg=colrBut)
+    Search_B.grid(row=5,column=2,pady=1,padx=1)
+
+    resultLabel_1=Label(root, text="Youtube Search",bg=colrLab,width=50)      #4
+    resultLabel_1.grid(row=6,columnspan=2,pady=5,padx=5)
+
+    Search_B=Button(root,text="Play",width=10,bg=colrBut)
+    Search_B.grid(row=6,column=2,pady=1,padx=1)
+
+    resultLabel_1=Label(root, text="Youtube Search",bg=colrLab,width=50)      #5
+    resultLabel_1.grid(row=7,columnspan=2,pady=5,padx=5)
+
+    Search_B=Button(root,text="Play",width=10,bg=colrBut)
+    Search_B.grid(row=7,column=2,pady=1,padx=1)
+
+    resultLabel_1=Label(root, text="Youtube Search",bg=colrLab,width=50)      #6
+    resultLabel_1.grid(row=8,columnspan=2,pady=5,padx=5)
+
+    Search_B=Button(root,text="Play",width=10,bg=colrBut)
+    Search_B.grid(row=8,column=2,pady=1,padx=1)
+
+    Download_B=Button(root,text="Download",command=Download,width=20,bg="#a2cf6e",bd=1)
+    Download_B.grid(row=9,column=1,pady=3,padx=3)
 
 Widgets()
 root.mainloop()  
